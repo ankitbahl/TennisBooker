89be7db7a3a752e9dd2834126b3af3bf6145198a
Ankit Bahl <ankit2bahl@gmail.com>
2024-11-12 23:36:51 -0800

    Add error piping to multibook

--- src/multiBook.sh
+++ src/multiBook.sh
@@ -2,6 +2,6 @@
 # spins multiple processes to book
 export NODE_PATH="$HOME/.nvm/versions/node/v20.18.0/bin/node"
 
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs1.txt &
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs2.txt &
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs3.txt &
+$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs1.txt 2>&1 &
+$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs2.txt 2>&1 &
+$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs3.txt 2>&1 &
