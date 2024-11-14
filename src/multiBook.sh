db3477d4be3f33ae77e627fa6cacec5f6b2bda5f
Ankit Bahl <abbahl@amazon.com>
2024-11-14 09:13:21 -0800

    change reload action and fix multibook

--- src/multiBook.sh
+++ src/multiBook.sh
@@ -2,8 +2,6 @@
 # spins multiple processes to book
 export NODE_PATH="$HOME/.nvm/versions/node/v20.18.0/bin/node"
 
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs1.txt 2>&1 &
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs2.txt 2>&1 &
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs3.txt 2>&1 &
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs4.txt 2>&1 &
-$NODE_PATH ~/workspace/TennisBooker/src/book.js >> ~/workspace/TennisBooker/logs/logs5.txt 2>&1 &
+for i in {1..10}; do
+  $NODE_PATH ~/workspace/TennisBooker/src/book.js "$i" >> "$HOME/workspace/TennisBooker/logs/logs$i.txt" 2>&1 &
+done
