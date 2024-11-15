9a49865d40728f574aa08fcb90d186d91226d4b1
Ankit Bahl <ankit2bahl@gmail.com>
2024-11-14 22:57:18 -0800

    only use 1 instance

--- src/multiBook.sh
+++ src/multiBook.sh
@@ -2,6 +2,6 @@
 # spins multiple processes to book
 export NODE_PATH="$HOME/.nvm/versions/node/v20.18.0/bin/node"
 
-for i in {1..10}; do
+for i in {1..1}; do
   $NODE_PATH ~/workspace/TennisBooker/src/book.js "$i" >> "$HOME/workspace/TennisBooker/logs/logs$i.txt" 2>&1 &
 done
