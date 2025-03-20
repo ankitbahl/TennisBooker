c38e3842732ffe5450e3528faed8d3e3e90309d2
Ankit Bahl <abbahl@amazon.com>
2025-03-20 15:12:32 -0700

    record session as video

--- src/book.ts
+++ src/book.ts
@@ -34,7 +34,11 @@ async function bookCourt(email: string) {
 
     for (let i = 0; i < 5; i++) {
         const browser = await chromium.launch({headless: true});
-        const context = await browser.newContext();
+        const context = await browser.newContext({
+            recordVideo: {
+                dir: 'videos/'
+            }
+        });
         const page = await context.newPage();
         // await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:131.0) Gecko/20100101 Firefox/131.0");
 
