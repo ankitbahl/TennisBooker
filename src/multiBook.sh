b13fc5dd0e4fb4a9e31b49431de8435ff0a89a88
Ankit Bahl <ankit2bahl@gmail.com>
2026-03-19 20:35:23 +0000

    Fix path for node and booker

--- src/multiBook.sh
+++ src/multiBook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export NODE_PATH="$HOME/.nvm/versions/node/v20.18.0/bin/node"
+export NODE_PATH="$HOME/.nvm/versions/node/v22.22.0/bin/node"
 
-$NODE_PATH /home/jellyfin/TennisBooker/build/src/book.js >> "$HOME/TennisBooker/logs/logs1.txt" 2>&1
+$NODE_PATH /home/jellyfin/projects/TennisBooker/build/src/book.js >> "$HOME/projects/TennisBooker/logs/logs1.txt" 2>&1
